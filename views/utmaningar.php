3953c1806a13b76a37bb4707f9e10d141969b7c9
Jamie Blomerus <jamie.blomerus@protonmail.com>
2024-01-10 12:07:32 +0100

    Begränsa utmaningar till autentiserade

--- views/utmaningar.php
+++ views/utmaningar.php
@@ -16,9 +16,18 @@ class Home {
             Actions::add_action('title', [$this, 'title']);
             get_header();
         }
+
+        $loggedin = \PBCTF\LoginAPI::is_logged_in();
         ?>
-        <main class="challenges">
+        <main class="challenges<?php echo !$loggedin ? ' disabled' : '' ?>" data-title="<?php $this->title() ?>">
             <h2>Utmaningar</h2>
+            <?php
+            if (!$loggedin) {
+                ?>
+                <p class="error">Du måste logga in för att komma åt utmaningarna</p>
+                <?php
+            }
+            ?>
             <div class="challenge-categories">
                 <div class="challenge-category">
                     <h3>Webb</h3>
