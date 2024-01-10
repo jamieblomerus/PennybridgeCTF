176488f8e6a90dc03aeadd580c6f5168e9cd1fba
Jamie Blomerus <jamie.blomerus@protonmail.com>
2024-01-10 12:08:21 +0100

    Uppdaterade header.php

--- views/components/header.php
+++ views/components/header.php
@@ -9,15 +9,33 @@ use PBCTF\Actions;
     <meta charset="UTF-8">
     <title><?php Actions::do_action('title'); ?></title>
     <link rel="stylesheet" href="/static/stylesheets/main.css">
+    <script src="/static/js/async-loading.js"></script>
     <?php Actions::do_action('head'); ?>
 </head>
 <body>
     <?php Actions::do_action('body_start'); ?>
     <nav>
-        <h1><a href="/">Pennybridge CTF</a></h1>
+        <h1><a class="async-loading" href="/">Pennybridge CTF</a></h1>
         <ul>
-            <li><a href="/utmaningar">Utmaningar</a></li>
-            <li><a href="/poangtavla">Poängtavla</a></li>
+            <li><a class="async-loading" href="/utmaningar">Utmaningar</a></li>
+            <li><a class="async-loading" href="/poangtavla">Poängtavla</a></li>
+            <?php
+            if (\PBCTF\LoginAPI::is_admin()) {
+                ?>
+                <li><a class="async-loading" href="/admin">Admin</a></li>
+                <?php
+            }
+            ?>
         </ul>
-        <a class="login" href="/login">Logga in</a>
+        <?php
+        if (\PBCTF\LoginAPI::is_logged_in()) {
+            ?>
+            <a class="login" href="/utloggning">Logga ut</a>
+            <?php
+        } else {
+            ?>
+            <a class="login" href="/inloggning">Logga in</a>
+            <?php
+        }
+        ?>
     </nav>
