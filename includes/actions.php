96cf1a5b366d459dc6b7a90b676dd0bcffb45cb5
Jamie Blomerus <jamie.blomerus@protonmail.com>
2024-01-11 12:02:11 +0100

    Tog bort Filters då de ej används

--- includes/actions.php
+++ includes/actions.php
@@ -18,27 +18,3 @@ class Actions {
         }
     }
 }
-
-class Filters {
-    private static array $filters = [];
-
-    public static function add_filter(string $filter, callable $callback, int $priority = 10) {
-        self::$filters[$filter][$priority][] = $callback;
-    }
-
-    public static function apply_filters(string $filter, $value, ...$args) {
-        if (!isset(self::$filters[$filter]) || !is_array(self::$filters[$filter])) {
-            return $value;
-        }
-
-        ksort(self::$filters[$filter]);
-
-        foreach (self::$filters[$filter] as $callbacks) {
-            foreach ($callbacks as $callback) {
-                $value = call_user_func_array($callback, array_merge([$value], $args));
-            }
-        }
-
-        return $value;
-    }
-}
