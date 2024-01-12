c8ac4f3c94f0368a85de72c6f938875f72955bb1
Jamie Blomerus <jamie.blomerus@protonmail.com>
2024-01-12 14:48:10 +0100

    Tog bort regler-länk

--- views/components/footer.php
+++ views/components/footer.php
@@ -4,7 +4,7 @@ namespace PBCTF\Views\Components;
 use PBCTF\Actions;
 ?>
 <footer class="site-footer">
-    <p>Detta projekt är skapat av <a href="https://github.com/jamieblomerus">Jamie Blomerus</a> och får enbart attackeras i enlighet med <a href="/regler">reglerna</a>, alla andra attacker utgör dataintrång.</p>
+    <p>Detta projekt är skapat av <a href="https://github.com/jamieblomerus">Jamie Blomerus</a> och får enbart attackeras i enlighet med reglerna, alla andra attacker utgör dataintrång.</p>
     <?php Actions::do_action('footer'); ?>
 </footer>
 <?php Actions::do_action('body_end'); ?>
